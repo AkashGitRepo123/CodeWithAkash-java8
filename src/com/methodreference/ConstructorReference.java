1b7cd8fa60c2c723514f4c71450602160278c096
satishsamp <satishesampate@gmail.com>
2022-03-03 17:51:28 +0530

    removed error.

--- src/com/methodreference/ConstructorReference.java
+++ src/com/methodreference/ConstructorReference.java
@@ -18,11 +18,9 @@ class Message {
 
 
 	String msg = "hi there!";
-<<<<<<< HEAD
 	
 	// this is my own comment to check the msg conflicts.
-=======
->>>>>>> 55d60195aaa81b157ac6ea44538b1bd25593b96c
+
 
 	private String messageFrom;
 
