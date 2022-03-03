4dcca19fe29dbbe5ed6948463d1561374717d558
satishsamp <100749909+satishsamp@users.noreply.github.com>
2022-03-03 15:50:20 +0530

    Feature dev 201  (#5)
    * Added errorCode field.
    
    * Adding messageTo field.

--- src/com/methodreference/ConstructorReference.java
+++ src/com/methodreference/ConstructorReference.java
@@ -20,6 +20,17 @@ class Message {
 	private int messageId;
 	
 	private int errorCode;
+	
+	public String getMessageTo() {
+		return messageTo;
+	}
+
+	public void setMessageTo(String messageTo) {
+		this.messageTo = messageTo;
+	}
+
+	private String messageTo;
+
 
 	public String getMsg() {
 		return msg;
