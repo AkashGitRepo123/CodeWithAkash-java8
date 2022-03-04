5d09ffe51a3e384a8ded5cc250ff52c5a992f019
satishsamp <satishesampate@gmail.com>
2022-03-04 10:45:08 +0530

    Added status field.

--- src/com/methodreference/ConstructorReference.java
+++ src/com/methodreference/ConstructorReference.java
@@ -28,6 +28,16 @@ class Message {
 	
 	private int errorCode;
 	
+	private String status;
+	
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 	public String getMessageTo() {
 		return messageTo;
 	}
